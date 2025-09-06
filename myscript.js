8e5566b830c8b645718f7f2ad7994f7738486194
thisisrakeshhhh <rakeshkumarbro2182004@gmail.com>
2025-09-06 19:13:18 +0530

    After that

--- myscript.js
+++ myscript.js
@@ -1,9 +1,9 @@
 let total = 0;
 let elements = 0;
 let product = 0;
-function addtocart(){
+function addtocart(a,b){
     product += 1;
-    total = 45000 * product;
+    total = a * product;
     document.getElementById("showproduct").innerHTML = "No of products: " + product;
     document.getElementById("Total").innerHTML = "Total: ₹" + total;
 }
