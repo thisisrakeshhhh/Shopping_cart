7568ca443317a60a0d9c7553e48cd796a36c08fc
thisisrakeshhhh <rakeshkumarbro2182004@gmail.com>
2025-09-08 14:46:50 +0530

    MY commit to make it smoot

--- myscript.js
+++ myscript.js
@@ -1,31 +1,59 @@
-var product1 = 0;
-var product2 = 0;
-var product3 = 0;
-var total = 0;
+    var product1 = 0;
+    var product2 = 0;
+    var product3 = 0;
+    var total = 0;
 
-function addtocart1(name, price){
-    product1++;
-    total += price;
-    document.getElementById("showproduct1").innerHTML = "No of products: " + product1 + "" + name;
-    document.getElementById("Total").innerHTML = "Total: ₹" + total;
-    // alert("Item added to cart");
-    document.getElementById("itemadded1").innerHTML = name + "Price: ₹" + price;
-}
+    function updateTotal() {
+      document.getElementById("Total").innerHTML = "Total: ₹" + total;
+    }
 
-function addtocart2(name, price){
-    product2 = 1;
-    total += price;
-    document.getElementById("showproduct2").innerHTML = "No of products: " + product2 + "" + name;
-    document.getElementById("Total").innerHTML = "Total: ₹" + total;
-    // alert("Item added to cart");
-    document.getElementById("itemadded2").innerHTML = name + "Price: ₹" + price;
-}
+    function addtocart1(name, price) {
+      product1++;
+      total += price;
+      document.getElementById("showproduct1").innerHTML = "No of " + name + ": " + product1;
+      document.getElementById("itemadded1").innerHTML = name + " | Price: ₹" + price;
+      updateTotal();
+    }
+    function remove1(name, price) {
+      if (product1 > 0) {
+        product1--;
+        total -= price;
+        document.getElementById("showproduct1").innerHTML = "No of " + name + ": " + product1;
+        if (product1 === 0) document.getElementById("itemadded1").innerHTML = "";
+        updateTotal();
+      }
+    }
 
-function addtocart3(name, price){
-    product3 = 1;
-    total += price;
-    document.getElementById("showproduct3").innerHTML = "No of products: " + product3 + "" + name;
-    document.getElementById("Total").innerHTML = "Total: ₹" + total;
-    // alert("Item added to cart");
-    document.getElementById("itemadded3").innerHTML = name + "Price: ₹" + price;
-}
+    function addtocart2(name, price) {
+      product2++;
+      total += price;
+      document.getElementById("showproduct2").innerHTML = "No of " + name + ": " + product2;
+      document.getElementById("itemadded2").innerHTML = name + " | Price: ₹" + price;
+      updateTotal();
+    }
+    function remove2(name, price) {
+      if (product2 > 0) {
+        product2--;
+        total -= price;
+        document.getElementById("showproduct2").innerHTML = "No of " + name + ": " + product2;
+        if (product2 === 0) document.getElementById("itemadded2").innerHTML = "";
+        updateTotal();
+      }
+    }
+
+    function addtocart3(name, price) {
+      product3++;
+      total += price;
+      document.getElementById("showproduct3").innerHTML = "No of " + name + ": " + product3;
+      document.getElementById("itemadded3").innerHTML = name + " | Price: ₹" + price;
+      updateTotal();
+    }
+    function remove3(name, price) {
+      if (product3 > 0) {
+        product3--;
+        total -= price;
+        document.getElementById("showproduct3").innerHTML = "No of " + name + ": " + product3;
+        if (product3 === 0) document.getElementById("itemadded3").innerHTML = "";
+        updateTotal();
+      }
+    }
